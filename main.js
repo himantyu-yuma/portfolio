b696da0281893ca73ad28ca128afd25420cc00dd
aroe <lipton.happy.milktea@gmail.com>
2019-09-19 23:34:15 +0900

    modify

--- main.js
+++ main.js
@@ -8,17 +8,13 @@ $.ajax({
 // Ajaxリクエストが成功した時発動
 .done( (data) => {
     worksData = data;
-
-})
-// Ajaxリクエストが失敗した時発動
-.fail( (data) => {
-    console.log('error')
-})
-
-$(function(){
     $('#game').append(
         $('<h3>',{
             text: worksData.game_apps[0].title
         })
     )
+})
+// Ajaxリクエストが失敗した時発動
+.fail( (data) => {
+    console.log('error')
 })
