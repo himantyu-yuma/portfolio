a549367d6b73c084d445fd9c58815fab51b464de
himantyu-yuma <himantyu.and.gato@gmail.com>
2019-09-19 23:55:52 +0900

    modify js

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 let worksData;
 
 $.ajax({
-    url:'./works.json',
+    url: './works.json',
     async: true,
     dataType: "json",
 })
-// Ajaxリクエストが成功した時発動
-.done( (data) => {
-    worksData = data;
-    $('#game').append(
-        $('<h3>',{
-            text: worksData.game_apps[0].title
-        })
-    )
-})
-// Ajaxリクエストが失敗した時発動
-.fail( (data) => {
-    console.log('error')
-})
+    // Ajaxリクエストが成功した時発動
+    .done((data) => {
+        worksData = data;
+        $('#game').append(
+            $('<h3/>').append('<a>', {
+                text: worksData.game_apps[0].title
+            }).append(
+                    $('<p>').text(data.description)
+                ).append(
+                    $('<div>', {
+                        class: 'row'
+                    })
+                )
+        )
+    })
+    // Ajaxリクエストが失敗した時発動
+    .fail((data) => {
+        console.log('error')
+    });
