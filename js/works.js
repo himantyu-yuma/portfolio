c0d07e5b10f599edf7233e7e65943af765bb6514
himantyu-yuma <lipton.happy.milktea@gmail.com>
2024-06-17 01:08:55 +0900

    update: 作品のリンクなどを修正

--- js/works.js
+++ js/works.js
@@ -22,10 +22,6 @@ export const webWorks = [
     "description": "技育祭2021用に制作した、講演毎のTweetがまとめて見られるサイト",
     "image": "image/geek.png",
     "links": [
-      {
-        "name": "link",
-        "url": "https://twist-geek.herokuapp.com/"
-      }
     ]
   },
   {
@@ -34,7 +30,7 @@ export const webWorks = [
     "image": "image/miteru.png",
     "links": [
       {
-        "name": "link",
+        "name": "paper",
         "url": "https://hai-conference.net/symp2021/proceedings/html/paper/paper-G-14.html"
       }
     ]
@@ -45,7 +41,7 @@ export const webWorks = [
     "image": "image/reporthelper.png",
     "links": [
       {
-        "name": "link",
+        "name": "資料",
         "url": "https://docs.google.com/presentation/d/1jjgLMoTBOnfpMXzCpWOdQFDWL1zCv4ImsTzcHEIBnxg/edit?usp=sharing"
       }
     ]
@@ -56,7 +52,7 @@ export const webWorks = [
     "image": "image/netsuzou.png",
     "links": [
       {
-        "name": "link",
+        "name": "資料",
         "url": "https://docs.google.com/presentation/d/1q7cGQsjMIVmUa4fTp6Tsoesxqsl6d2PvKTfkkEJkLuQ/edit?usp=sharing"
       }
     ]
@@ -67,7 +63,7 @@ export const webWorks = [
     "image": "image/vrouka.png",
     "links": [
       {
-        "name": "link",
+        "name": "資料",
         "url": "https://docs.google.com/presentation/d/1jnR8nWh144R6d2Kw8l5GVfkhvoxceX7ROf1P629aM7U/edit?usp=sharing"
       }
     ]
@@ -78,7 +74,7 @@ export const webWorks = [
     "image": "image/familyreq.png",
     "links": [
       {
-        "name": "link",
+        "name": "資料",
         "url": "https://docs.google.com/presentation/d/11xrRzxVsF73Xlss_zw2qgAZZijWZxhxhmmwscX-VFHI/edit?usp=sharing"
       }
     ]
@@ -89,7 +85,7 @@ export const webWorks = [
     "image": "image/arhanabi.png",
     "links": [
       {
-        "name": "link",
+        "name": "資料",
         "url": "https://docs.google.com/presentation/d/1ZSYd9yZHoEvpZM3kmWDHit4_2IkMvJrv9lHZ9pP22k4/edit?usp=sharing"
       }
     ]
@@ -100,7 +96,7 @@ export const webWorks = [
     "image": "image/playfests.png",
     "links": [
       {
-        "name": "link",
+        "name": "資料",
         "url": "https://docs.google.com/presentation/d/1gBVFKUtO-l81_kU53hmNzSqoDvV3EELNoP73dyCM-ZE/edit?usp=sharing"
       }
     ]
@@ -111,7 +107,7 @@ export const webWorks = [
     "image": "image/manaba.png",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/494"
       }
     ]
@@ -122,11 +118,11 @@ export const webWorks = [
     "image": "video/timetable.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://timetable.417.works/"
       },
       {
-        "name": "article",
+        "name": "紹介記事",
         "url": "https://www.otaru-uc.ac.jp/news/141122/"
       }
     ]
@@ -137,11 +133,11 @@ export const webWorks = [
     "image": "video/jphacks.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "repository",
         "url": "https://github.com/jphacks/SP_1907"
       },
       {
-        "name": "article",
+        "name": "紹介記事",
         "url": "https://www.otaru-uc.ac.jp/news/19263/"
       }
     ]
@@ -152,8 +148,8 @@ export const webWorks = [
     "image": "video/lottery.mp4",
     "links": [
       {
-        "name": "article",
-        "url": "https://www.city.shizuoka.lg.jp/000835144.pdf"
+        "name": "イベント概要",
+        "url": "https://shizuokahappy.com/enjoy/event/25572/"
       }
     ]
   },
@@ -163,8 +159,8 @@ export const webWorks = [
     "image": "video/stamp-rally.mp4",
     "links": [
       {
-        "name": "article",
-        "url": "https://www.city.shizuoka.lg.jp/000835144.pdf"
+        "name": "イベント概要",
+        "url": "https://shizuokahappy.com/enjoy/event/25572/"
       }
     ]
   }
@@ -180,21 +176,17 @@ export const gameWorks = [
     "image": "video/yadokari.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/%e3%83%87%e3%82%b3%e3%83%aa%e3%83%a4%e3%83%89%e3%82%ab%e3%83%aa"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://github.com/himantyu-yuma/ggj-yadokari/releases/download/v0.5-alpha/win.zip"
       },
       {
-        "name": "mac",
+        "name": "mac用",
         "url": "https://github.com/himantyu-yuma/ggj-yadokari/releases/download/v0.5-alpha/mac.zip"
       },
-      {
-        "name": "video",
-        "url": "video/yadokari.mp4"
-      }
     ]
   },
   {
@@ -203,12 +195,8 @@ export const gameWorks = [
     "image": "video/dooropen.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "unityroom",
         "url": "https://unityroom.com/games/door-open"
-      },
-      {
-        "name": "video",
-        "url": "video/dooropen.mp4"
       }
     ]
   },
@@ -218,15 +206,15 @@ export const gameWorks = [
     "image": "image/voicebattle.png",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/%e5%a3%b0is%e3%83%91%e3%83%af%e3%83%bc"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://drive.google.com/file/d/1jyZ6zRWA514mF2fq0lNQDFmUibiIs7G5/view?usp=sharing&export=download"
       },
       {
-        "name": "mac",
+        "name": "mac用",
         "url": "https://drive.google.com/file/d/1z_s3DO1cg5wLfnTcwNI4TGuKJQIkPMB6/view?usp=sharing&export=download"
       }
     ]
@@ -237,20 +225,16 @@ export const gameWorks = [
     "image": "video/penguin.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/%e3%82%b0%e3%83%ab%e3%83%a1%e3%83%9a%e3%83%b3%e3%82%ae%e3%83%b3%e3%81%ae%e5%a4%a7%e5%86%92%e9%99%baii-delux-edition"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://drive.google.com/file/d/1UPG8RS1Qtw_5A1sV0cGL_QUK91n-DBR-/view?usp=sharing"
       },
       {
-        "name": "mac",
+        "name": "mac用",
         "url": "https://drive.google.com/file/d/1b9zSezXHdb-h4e1BJuD1fLmrqWTBRppQ/view?usp=sharing"
-      },
-      {
-        "name": "video",
-        "url": "video/penguin.mp4"
       }
     ]
   },
@@ -260,16 +244,12 @@ export const gameWorks = [
     "image": "video/novel.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/%e3%82%82%e3%81%97%e3%82%82%e3%80%81%e6%99%ae%e9%80%9a%e3%81%ae%e5%a5%b3%e5%ad%90%e5%ad%a6%e7%94%9f%e3%81%a7%e3%81%82%e3%82%8b%e7%a7%81%e3%81%8c%e7%be%8e%e5%b0%91%e5%a5%b3%e3%81%ab%e6%84%9b%e3%82%92"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://github.com/soukatsu-ouc-programmers/2020summer-novel/releases/download/v1.1/win.zip"
-      },
-      {
-        "name": "video",
-        "url": "video/novel.mp4"
       }
     ]
   },
@@ -279,20 +259,16 @@ export const gameWorks = [
     "image": "video/dangomushi.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/super-dangomushi"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://drive.google.com/file/d/1wgzxQB0hQtnT-a3JGZX_Dg0eBudS_bMT/view?usp=sharing"
       },
       {
-        "name": "mac",
+        "name": "mac用",
         "url": "https://drive.google.com/file/d/1PW0evaLfHMKlsNhhYjZWuiFFylBCVbAA/view?usp=sharing"
-      },
-      {
-        "name": "video",
-        "url": "video/dangomushi.mp4"
       }
     ]
   },
@@ -302,16 +278,12 @@ export const gameWorks = [
     "image": "video/kenja.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/ggj2020-kenja"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://ggj.s3.amazonaws.com/games/2020/02/247027/exec/jOWQ8/v3.zip"
-      },
-      {
-        "name": "video",
-        "url": "video/kenja.mp4"
       }
     ]
   },
@@ -321,12 +293,8 @@ export const gameWorks = [
     "image": "video/horror.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "unityroom",
         "url": "https://unityroom.com/games/horrorff"
-      },
-      {
-        "name": "video",
-        "url": "video/horror.mp4"
       }
     ]
   },
@@ -336,16 +304,12 @@ export const gameWorks = [
     "image": "video/chopstick.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/chopstick"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://drive.google.com/open?id=1hhA6zs_9KrQOBqfuLk7tF0PuZSQ2tS3k"
-      },
-      {
-        "name": "video",
-        "url": "video/chopstick.mp4"
       }
     ]
   },
@@ -355,12 +319,8 @@ export const gameWorks = [
     "image": "video/vrouc.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "公式サイト",
         "url": "http://virtual-ouc.net/lp"
-      },
-      {
-        "name": "video",
-        "url": "video/vrouc.mp4"
       }
     ]
   },
@@ -370,12 +330,8 @@ export const gameWorks = [
     "image": "video/traffic-safety.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "紹介記事",
         "url": "https://www.otaru-uc.ac.jp/event/6905/"
-      },
-      {
-        "name": "video",
-        "url": "video/traffic-safety.mp4"
       }
     ]
   },
@@ -385,20 +341,16 @@ export const gameWorks = [
     "image": "video/wos.mp4",
     "links": [
       {
-        "name": "link",
+        "name": "概要",
         "url": "https://soukatsu-ouc.com/works/world-of-snowplows"
       },
       {
-        "name": "win",
+        "name": "Windows用",
         "url": "https://drive.google.com/uc?export=download&id=1hhfjr1uou0-nmBCy9A4A2u8w99MK6h_R"
       },
       {
-        "name": "mac",
+        "name": "mac用",
         "url": "https://drive.google.com/uc?export=download&id=1fzhQMfk3w19gnMlSBJAStppqHRO61R_H"
-      },
-      {
-        "name": "video",
-        "url": "video/wos.mp4"
       }
     ]
   },
@@ -408,12 +360,8 @@ export const gameWorks = [
     "image": "video/cube-simulator.mp4",
     "links": [
       {
-        "name": "win",
+        "name": "windows用",
         "url": "https://drive.google.com/uc?export=download&id=1h94-PIjKp1bC-OQPvUmEXjj3qtVnq0qi"
-      },
-      {
-        "name": "video",
-        "url": "video/cube-simulator.mp4"
       }
     ]
   }
@@ -472,10 +420,6 @@ export const videoWorks = [
     "description": "高校時代に作った動画その2",
     "image": "video/beam.mp4",
     "links": [
-      {
-        "name": "video",
-        "url": "video/beam.mp4"
-      }
     ]
   },
   {
@@ -483,10 +427,6 @@ export const videoWorks = [
     "description": "高校時代に作った動画その1",
     "image": "video/battle.mp4",
     "links": [
-      {
-        "name": "video",
-        "url": "video/battle.mp4"
-      }
     ]
   }
 ];
